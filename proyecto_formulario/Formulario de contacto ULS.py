ee097184ce7cdcf017461e86723bbcaa3df98cae
JPablo1235 <j_pablo1235@petalmail.com>
2025-04-06 13:41:48 -0500

    Se agrega el archivo requirements.txt para poder ser publicado en Render.com Se hacen modificaciones en el archivo principal de la aplicacion para que pueda correr en el servidor de Render.com

--- proyecto_formulario/Formulario de contacto ULS.py
+++ proyecto_formulario/Formulario de contacto ULS.py
@@ -12,42 +12,46 @@ from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
 from crear_bdd import crear_base_datos  # Importa la función desde crear_bdd.py
 
-app = Flask(__name__)
+def crear_app():
 
-# Funcion para crear la base de datos y la tabla si no existen
-crear_base_datos()
+    app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html') # Página de inicio
+    # Funcion para crear la base de datos y la tabla si no existen
+    crear_base_datos()
 
-@app.route('/contacto') # Ruta para el formulario de contacto
-def contacto():
-    return render_template('contacto.html')
+    @app.route('/')
+    def index():
+        return render_template('index.html') # Página de inicio
 
-@app.route('/submit_contacto', methods=['POST']) # Funcion para manejar el envío del formulario
-def submit_contacto():
-    nombre = request.form['nombre']
-    email = request.form['email']
-    mensaje = request.form['mensaje']
-    
-    # Conectar a la base de datos y agregar el nuevo contacto
-    conn = sqlite3.connect('database.db')
-    cursor = conn.cursor()
-    cursor.execute('INSERT INTO contactos (nombre, email, mensaje) VALUES (?, ?, ?)', (nombre, email, mensaje))
-    conn.commit()
-    conn.close()
-    
-    return redirect(url_for('mostrar_datos'))  # Redirigir a la página que muestra los datos
+    @app.route('/contacto') # Ruta para el formulario de contacto
+    def contacto():
+        return render_template('contacto.html')
 
-@app.route('/mostrar_datos') # Ruta para mostrar los datos almacenados
-def mostrar_datos():
-    conn = sqlite3.connect('database.db')
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM contactos') # Consulta para obtener todos los contactos
-    contactos = cursor.fetchall()
-    conn.close()
-    return render_template('mostrar_datos.html', contactos=contactos)
+    @app.route('/submit_contacto', methods=['POST']) # Funcion para manejar el envío del formulario
+    def submit_contacto():
+        nombre = request.form['nombre']
+        email = request.form['email']
+        mensaje = request.form['mensaje']
+        
+        # Conectar a la base de datos y agregar el nuevo contacto
+        conn = sqlite3.connect('database.db')
+        cursor = conn.cursor()
+        cursor.execute('INSERT INTO contactos (nombre, email, mensaje) VALUES (?, ?, ?)', (nombre, email, mensaje))
+        conn.commit()
+        conn.close()
+        
+        return redirect(url_for('mostrar_datos'))  # Redirigir a la página que muestra los datos
+
+    @app.route('/mostrar_datos') # Ruta para mostrar los datos almacenados
+    def mostrar_datos():
+        conn = sqlite3.connect('database.db')
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM contactos') # Consulta para obtener todos los contactos
+        contactos = cursor.fetchall()
+        conn.close()
+        return render_template('mostrar_datos.html', contactos=contactos)
+    return app
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app = crear_app() # Crear la aplicación Flask para correrla en Render.com
+    app.run()  # Ejecutar la aplicación Flask
